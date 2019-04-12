ebc4e8996be0b02f2a335fdbe20b943b91594e1f
Christoph Stepper <christoph.stepper@gfk.com>
2019-04-12 11:24:54 +0200

    added another saying: reality

--- data-raw/irfans_sayings.R
+++ data-raw/irfans_sayings.R
@@ -4,7 +4,8 @@ sayings =
     "desaster" =  "This is the recipe of desaster.",
     "possibe" = "Everything is possible, if you have time and money.",
     "coffee" = "Never say NO to free coffee.",
-    "tomtom" = "Please always check carefully, because that's Tomtom."
+    "tomtom" = "Please always check carefully, because that's Tomtom.",
+    "reality" = "Unfortunately, this is reality."
   )
 
 usethis::use_data(sayings, overwrite = TRUE)
