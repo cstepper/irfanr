8ca02f751a17ef9e755fdf69108f6003289b2192
Christoph Stepper <christoph.stepper@gfk.com>
2019-06-18 15:55:31 +0200

    added some new sayings

--- data-raw/irfans_sayings.R
+++ data-raw/irfans_sayings.R
@@ -5,7 +5,9 @@ sayings =
     "possibe" = "Everything is possible, if you have time and money.",
     "coffee" = "Never say NO to free coffee.",
     "tomtom" = "Please always check carefully, because that's Tomtom.",
-    "reality" = "Unfortunately, this is reality."
+    "reality" = "Unfortunately, this is reality.",
+    "mood" = "You are in funny mood today",
+    "error" = "Humans make errors, but angels not"
   )
 
 usethis::use_data(sayings, overwrite = TRUE)
